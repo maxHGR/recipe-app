c0da815eb7c0b3abeec7092c98813ae96a9e02e5
maxHGR <maxhgr2@gmail.com>
2022-08-28 01:44:40 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
-import "./style.css";
+import React, { useEffect, useState } from 'react';
+import './style.css';
 
-export default function App() {
+const App = () => {
+  const APP_ID = '278f2db2';
+  const APP_KEY = 'ac5aa8bc7602562e4fe71590cc8d8f15';
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
+
+export default App;
