3c618378804b136044cc6d345c094169a329902b
maxHGR <maxhgr2@gmail.com>
2022-08-28 19:57:15 +0200

    1 file modified

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import style from './recipe.module.css';
 
-
-const Recipe = ({ title, calories,  image, ingredients }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
   return (
     <div className={style.recipe}>
       <h1>{title}</h1>
       <ul>
-        {ingredients.map(ingredient => (
+        {ingredients.map((ingredient) => (
           <li>{ingredient.text}</li>
         ))}
       </ul>
       <p>
-        {calories.toFixed(2) + " kcal"}
+        {calories.toFixed(2) + ' kcal'}
       </p>
       <img className={style.image} src={image} alt="" />
-
     </div>
   );
 };
